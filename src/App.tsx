e2d1154124505212e0a8b9245c8604696af46025
Ivan Trandin <trandinii@gmail.com>
2023-11-07 22:32:10 +0400

    Microtasks s1w1 task #2

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,29 @@
-import React from 'react';
 import './App.css';
-import {TaskPropsType, ToDoList} from "./components/todoList/ToDoList";
+import {NewComponent} from "./components/newComponent/NewComponent";
+
+const students = [
+    {id: 1, name: "James", age: 8},
+    {id: 2, name: "Robert", age: 18},
+    {id: 3, name: "John", age: 28},
+    {id: 4, name: "Michael", age: 38},
+    {id: 5, name: "William", age: 48},
+    {id: 6, name: "David", age: 58},
+    {id: 7, name: "Richard", age: 68},
+    {id: 8, name: "Joseph", age: 78},
+    {id: 9, name: "Thomas", age: 88},
+    {id: 10, name: "Charles", age: 98},
+    {id: 11, name: "Christopher", age: 100},
+]
+
+const topCars = [
+    {manufacturer: 'BMW', model: 'm5cs'},
+    {manufacturer: 'Mersedes', model: 'e63s'},
+    {manufacturer: 'Audi', model: 'rs6'}
+]
 
 function App() {
-    const todoListTitle1: string = 'What to learn';
-    const todoListTitle2: string = 'What to buy'
-
-    const tasks_1: Array<TaskPropsType> = [
-        {id: 1, title: 'HTML', isDone: true},
-        {id: 2, title: 'JS/ES6', isDone: false},
-        /*{id: 3, title: 'React', isDone: false}*/
-    ]
-
-    const tasks_2: Array<TaskPropsType> = [
-        {id: 4, title: 'Beer', isDone: true},
-        {id: 5, title: 'Dried Fish', isDone: false},
-        {id: 6, title: 'Cheeps', isDone: false},
-        {id: 7, title: 'Cheeps', isDone: false},
-        {id: 8, title: 'Cheeps', isDone: false}
-    ]
-
     return (
-        <div className="App">
-            <ToDoList title={todoListTitle1} tasks={tasks_1}/>
-            {/*ToDolist(
-                {
-                    title: "What to learn",
-                    tasks: tasks_1
-                }
-            )*/}
-            <ToDoList title={todoListTitle2} tasks={tasks_2}/>
-        </div>
+        <NewComponent students={students} topCars={topCars}/>
     );
 }
 
